0835170ca2bf91104770382ceab192f5bc03347c
dsm-3 <di.silvargt@gmail.com>
2023-05-25 19:50:46 -0300

    cadastro usuario com mongodb funcionando

--- conexao.php
+++ conexao.php
@@ -1,4 +1,26 @@
 <?php
-header("Access-Control-Allow-Origin:*");
-$con=mysqli_connect("localhost","root","","DandelionPlatform")or die("Não pode conectar!");
+// Arquivo de conexão com o banco
+require 'vendor/autoload.php'; // Dependência do MongoDB
+// Importa as classes necessárias do MongoDB
+use MongoDB\Client;
+use MongoDB\Database;
+
+// Configurações de conexão com o MongoDB
+$mongoDBUri = "mongodb://localhost:27017"; // URI de conexão do MongoDB
+$mongoDBNome = "DonationPlatform"; // Nome do seu banco de dados MongoDB
+$colecaoUsuario = "usuario"; // Nome da coleção onde os dados dos produtos são armazenados
+// $colecaoFavoritos = "favoritos";
+
+// Cria uma instância do cliente do MongoDB
+$cliente = new Client($mongoDBUri);
+
+// Seleciona o banco de dados
+$bancoDeDados = $cliente->selectDatabase($mongoDBNome);
+
+// Seleciona a coleção
+$colecaoUsuario = $bancoDeDados->selectCollection($colecaoUsuario);
+// $colecaoFav = $bancoDeDados->selectCollection($colecaoFavoritos);
+
+global $colecaoUsuarios;
+// global $colecaoFav;
 ?>
