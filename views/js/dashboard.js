1809fc3627fbb1a53cdc8a201c14e6c50f170634
phdmhenrique <pablo.monteirorgt19@gmail.com>
2023-06-01 18:25:42 -0300

    Att. Dashboard User

---
+++ views/js/dashboard.js
@@ -0,0 +1,57 @@
+const requestsProducts = document.querySelectorAll(".requests-products");
+
+requestsProducts.forEach((requestsProduct) => {
+  const buttons = requestsProduct.querySelectorAll(".user-solicites__button");
+  const views = requestsProduct.querySelectorAll(".view-card__info");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const target = button.getAttribute("data-target");
+
+      views.forEach((view) => {
+        if (view.getAttribute("id") === target) {
+          view.classList.add("active");
+        } else {
+          view.classList.remove("active");
+        }
+      });
+
+      buttons.forEach((btn) => {
+        if (btn === button) {
+          btn.classList.add("active");
+        } else {
+          btn.classList.remove("active");
+        }
+      });
+
+      const targetView = requestsProduct.querySelector(target);
+      if (targetView) {
+        targetView.classList.add("active");
+      }
+    });
+  });
+});
+
+// Obtém os botões e os elementos a serem exibidos
+const buttons = document.querySelectorAll('.user-solicited__button');
+const cards = document.querySelectorAll('.view-card__process');
+
+// Adiciona o evento de clique aos botões
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remove a classe 'active' de todos os botões e elementos
+    buttons.forEach(btn => btn.classList.remove('active'));
+    cards.forEach(card => card.classList.remove('active'));
+
+    // Adiciona a classe 'active' ao botão clicado
+    button.classList.add('active');
+
+    // Obtém o elemento a ser exibido com base no atributo 'data-target' do botão
+    const targetId = button.getAttribute('data-target');
+    const targetCard = document.querySelector(targetId);
+
+    // Adiciona a classe 'active' ao elemento a ser exibido
+    targetCard.classList.add('active');
+  });
+});
+
