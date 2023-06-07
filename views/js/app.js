f168c0e1b60f61cf1b2036d51c491c53c1302fdd
phdmhenrique <pablo.monteirorgt19@gmail.com>
2023-06-07 02:10:52 -0300

    Corrigindo a verificação de senhas no cadastro do usuário para que as senhas sejam iguais.

--- views/js/app.js
+++ views/js/app.js
@@ -1,32 +1,43 @@
-// Obtém o elemento de input, o bloco de visualização e a imagem padrão
-var input = document.getElementById('upload-input');
-var preview = document.querySelector('.upload-preview');
-var defaultImage = document.getElementById('default-image');
+// CÓDIGO JAVASCRIPT PARA MOSTRAR A IMAGEM SELECIONADA NA DIV.
+const input = document.getElementById("upload-input");
+const preview = document.querySelector(".upload-preview");
+const defaultImage = document.getElementById("default-image");
 
-// Adiciona um evento de change no input
-input.addEventListener('change', function(e) {
-  // Obtém o arquivo selecionado
-  var file = e.target.files[0];
+input.addEventListener("change", function (e) {
+  const file = e.target.files[0];
 
-  // Cria um objeto FileReader
-  var reader = new FileReader();
+  const reader = new FileReader();
 
-  // Define a função de callback quando a imagem estiver carregada
-  reader.onload = function(e) {
-    // Cria um elemento de imagem
-    var img = document.createElement('img');
+  reader.onload = function (e) {
+    const img = document.createElement("img");
     img.src = e.target.result;
 
-    // Limpa o bloco de visualização e adiciona a imagem
-    preview.innerHTML = '';
+    preview.innerHTML = "";
     preview.appendChild(img);
   };
 
-  // Lê o arquivo como uma URL de dados
   reader.readAsDataURL(file);
 });
 
-// Verifica se nenhuma imagem foi selecionada e exibe a imagem padrão
 if (!input.value) {
-  defaultImage.style.display = 'block';
+  defaultImage.style.display = "block";
 }
+
+// VERIFICANDO SE AS SENHAS SÃO IGUAIS.
+// VERIFICANDO SE AS SENHAS SÃO IGUAIS.
+function validarFormulario() {
+  const senha = document.getElementById("senha").value;
+  const repetirSenha = document.getElementById("repetir-senha").value;
+  const msgError = document.querySelector(".error__equal-password");
+
+  if (senha !== repetirSenha) {
+    msgError.classList.remove("active");
+    void msgError.offsetWidth;
+    msgError.classList.add("active"); 
+
+    return false;
+  }
+
+  return true;
+}
+
